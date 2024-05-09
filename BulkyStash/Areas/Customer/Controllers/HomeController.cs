cf8d4ca59e2a027c6efb53314e992d3c4318c67b
Sinan <sinan.cakal00@gmail.com>
2024-05-09 18:56:48 +0300

    Added details controller for product

--- BulkyStash/Areas/Customer/Controllers/HomeController.cs
+++ BulkyStash/Areas/Customer/Controllers/HomeController.cs
@@ -24,6 +24,14 @@ namespace BulkyStash.Areas.Customer.Controllers
             return View(productList);
         }
 
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == id,IncludeProp: "Category");
+
+            return View(product);
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
