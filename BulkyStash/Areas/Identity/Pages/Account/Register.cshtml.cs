5dcad03c1886e7c15dfa1c73b31e5b451702e069
Sinan <sinan.cakal00@gmail.com>
2024-05-10 15:39:35 +0300

    Updated UI and added name and address

--- BulkyStash/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyStash/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -110,6 +110,15 @@ namespace BulkyStash.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
 
 
@@ -147,6 +156,14 @@ namespace BulkyStash.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                
+                user.Name = Input.Email;
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
