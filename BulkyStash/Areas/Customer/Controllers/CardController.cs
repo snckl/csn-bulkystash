7f7a7e31ab0e763f9ffd08b254f540c0ac650da1
Sinan <sinan.cakal00@gmail.com>
2024-05-15 14:54:54 +0300

    Added Summary UI and Plus Minus and Remove for ShoppingCard

--- BulkyStash/Areas/Customer/Controllers/CardController.cs
+++ BulkyStash/Areas/Customer/Controllers/CardController.cs
@@ -7,6 +7,8 @@ using System.Security.Claims;
 
 namespace BulkyStash.Areas.Customer.Controllers
 {
+    [Area("Customer")]
+    [Authorize]
     public class CardController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -17,11 +19,10 @@ namespace BulkyStash.Areas.Customer.Controllers
         }
 
 
-        [Area("Customer")]
-        [Authorize]
+        
         public IActionResult Index()
         {
-            var claimsIdentity = (ClaimsIdentity) User.Identity;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
 
@@ -30,7 +31,7 @@ namespace BulkyStash.Areas.Customer.Controllers
                 ShoppingCardList = _unitOfWork.ShoppingCard.GetAll(u => u.ApplicationUserId == userId, IncludeProp: "Product")
             };
 
-            foreach(var card in ShoppingCardVM.ShoppingCardList)
+            foreach (var card in ShoppingCardVM.ShoppingCardList)
             {
                 card.Price = GetPriceBasedOnQuantity(card);
                 ShoppingCardVM.OrderTotal += (card.Price * card.Count);
@@ -39,17 +40,62 @@ namespace BulkyStash.Areas.Customer.Controllers
             return View(ShoppingCardVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+        public IActionResult Plus(int cardId)
+        {
+            var cardFromDb = _unitOfWork.ShoppingCard.Get(u => u.Id == cardId);
+            cardFromDb.Count += 1;
+            _unitOfWork.ShoppingCard.Update(cardFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cardId)
+        {
+            var cardFromDb = _unitOfWork.ShoppingCard.Get(u => u.Id == cardId);
+            if (cardFromDb.Count <= 1)
+            {
+                _unitOfWork.ShoppingCard.Remove(cardFromDb);
+            }
+            else
+            {
+                cardFromDb.Count -= 1;
+                _unitOfWork.ShoppingCard.Update(cardFromDb);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cardId)
+        {
+            var cardFromDb = _unitOfWork.ShoppingCard.Get(u => u.Id == cardId);
+            _unitOfWork.ShoppingCard.Remove(cardFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
         private double GetPriceBasedOnQuantity(ShoppingCard shoppingCard)
         {
-            if(shoppingCard.Count <= 50)
+            if (shoppingCard.Count <= 50)
             {
                 return shoppingCard.Product.Price;
-            } else
+            }
+            else
             {
-                if(shoppingCard.Count <= 100)
+                if (shoppingCard.Count <= 100)
                 {
                     return shoppingCard.Product.Price50;
-                }else
+                }
+                else
                 {
                     return shoppingCard.Product.Price100;
                 }
