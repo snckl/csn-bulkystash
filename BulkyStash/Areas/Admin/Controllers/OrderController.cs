be20e3f89317678beefddca75dc5cb238b2e450d
Sinan <sinan.cakal00@gmail.com>
2024-05-16 20:41:32 +0300

    Added order controller and UI

---
+++ BulkyStash/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyStash.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;	
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(IncludeProp: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
+		}
+
+		#endregion
+	}
+}
